91fe2b6de48c72c036b5bef809c9a25064baa5a8
dave stronghold <103134380+dave-stronghold@users.noreply.github.com>
2023-06-15 07:56:46 +0000

    Update 3 files

--- src/pages/services/[slug].js
+++ src/pages/services/[slug].js
@@ -18,7 +18,7 @@ function ServiceDetailsPage({
     return (
         <>
             <Head>
-                <title>{service.title} - Oxybuild</title>
+                <title>{service.title} -services</title>
                 <meta
                     name="description"
                     content="OxyBuild - Construction React Next JS Template Industry, Products Manufacturing Company, building companies, architecture firms, and the like can take to their advantage by using OxyBuild - Construction React Next JS Template."
